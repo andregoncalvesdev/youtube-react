84f103ad2252190402b4944da9bb2146a7b5260c
André Gonçalves <andre.goncalves@tekzenit.com>
2016-02-03 11:37:21 +0000

    wip

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAuu3VRFWAJ3VG99DsC2uhxhVQOgmCm6Mo';
 
-//Create  a new component. this component shoud produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={ this.state.videos[0] } />
+        <VideoList videos = { this.state.videos } />
+      </div>
+    );
+  }
 }
 
 //Render on the DOM
